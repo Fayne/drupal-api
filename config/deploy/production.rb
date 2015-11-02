bdb548b6e2605b1290220c566980d71f67295d0e
Djamel Feddad <de.feddad@gmail.com>
2015-11-02 15:51:00 +0800

    Added Capistrano production server configuration

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,11 +1,21 @@
-# set :deploy_to, '/var/www/staging/drupal_api'
+role :app, %w{139.196.50.49}
+
+set :deploy_to, '/home/wppcampus/cms'
+
+set :settings_path, "#{fetch(:deploy_to)}/shared/sites/default"
 
 # set a branch for this release
-# set :branch, 'develop'
+set :branch, 'develop'
+
+set :app_environment, 'staging'
 
 SSHKit.config.command_map[:composer] = "#{shared_path.join("composer.phar")}"
 SSHKit.config.command_map[:drush] = "#{shared_path.join("vendor/bin/drush")}"
 
-server '192.168.10.10',
-user: 'vagrant'
-# roles: %w{'192.168.10.10'},
+set :file_permissions_paths, ["sites/default/files"]
+set :file_permissions_users, ["wppcampus"]
+set :file_permissions_chmod_mode, "0777"
+
+server '139.196.50.49',
+user: 'wppcampus',
+roles: %w{'139.196.50.49'}
